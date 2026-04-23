2a04e2859ebecbdefaaa98ff791d78d87d69f1ba
Vittorio Guerriero <vittorio.gue@gmail.com>
2026-04-23 07:59:46 +0100

    release: v0.0.37

--- scripts/install-dashboard.sh
+++ scripts/install-dashboard.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # VERSION is set locally by scripts/release.sh before tagging.
-VERSION="0.0.36"
+VERSION="0.0.37"
 REPO="uittorio/mentor-plugin"
 
 BINARY="${HOME}/.local/bin/mentor-dashboard"
